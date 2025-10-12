14ce163e939e1dda00649eb4447624341afc44ed
Peter Guy <peter.guy@sourcegraph.com>
2025-10-11 22:19:17 -0700

    Add the surface context to the Surface's setParent In order to set the private context variable so that initiSurface can use it.

--- src/apprt/gtk/class/surface.zig
+++ src/apprt/gtk/class/surface.zig
@@ -584,6 +584,7 @@ pub const Surface = extern struct {
     pub fn setParent(
         self: *Self,
         parent: *CoreSurface,
+        context: apprt.surface.NewSurfaceContext,
     ) void {
         const priv = self.private();
 
@@ -593,6 +594,9 @@ pub const Surface = extern struct {
             log.warn("setParent called after surface is already realized", .{});
             return;
         }
+
+        // Store the context so initSurface can use it
+        priv.context = context;
 
         // Setup our font size
         const font_size_ptr = glib.ext.create(font.face.DesiredSize);
